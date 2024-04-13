afcb60f29536e84d96764492d75623d4184ff2c1
Samyra Rocha <araujorocha.samyra@gmail.com>
2024-04-12 22:15:42 -0300

    Sr Reveal

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-window.sr = ScrollReveal({ reset: true});
+window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.area-1, .area-2, .area-3', {
     rotate: {x:100, y:100, z:0},
     duration: 1500,
- })
+ });
